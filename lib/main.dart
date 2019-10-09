09a998c78a4e78db9c703317191edae45406daab
MithoonSharechat <mithoon@sharechat.co>
2019-10-09 12:47:04 +0530

    lesson-6

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('Hello Flutter'),
       centerTitle: true,
+      backgroundColor: Colors.red[500],
     ),
     body: Center(
-      child: Text("Content of the body"),
+      child: Text(
+        "Body of the app",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'Mansalava'
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       child: Text('click'),
+      onPressed: () {},
+      backgroundColor: Colors.red[500],
     ),
   )
 ));
