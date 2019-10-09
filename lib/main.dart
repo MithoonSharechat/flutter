e61ddcdd35c07d33324e945de89f83152e9c9528
MithoonSharechat <mithoon@sharechat.co>
2019-10-09 19:54:22 +0530

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: Home(),
 ));
-
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello Flutter'),
-        centerTitle: true,
-        backgroundColor: Colors.red[500],
+          title: Text('Hello Flutter'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600]
       ),
       body: Center(
-        child: Text(
-          "Body of the app hot reload",
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'Mansalava'
-          ),
+        child: Image(
+          image: NetworkImage("https://images.unsplash.com/photo-1539671795355-0fd422702e3a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"),
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
         child: Text('click'),
-        onPressed: () {},
-        backgroundColor: Colors.red[500],
       ),
     );
   }
